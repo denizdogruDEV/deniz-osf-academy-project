746aee8d10a1441bad268ff835ea3084c42deee2
Deniz <ddogru09@gmail.com>
2021-01-17 22:53:31 +0300

    release page

--- src/pages/releasePage/releasePage.js
+++ src/pages/releasePage/releasePage.js
@@ -1,9 +1,52 @@
 import React from "react";
-function Release() {
-    return (
-        <>
-        </>
-        );
-}
+import Countdown from "../../components/countDownComp/countDownComp";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import "./releasePage.scss"
 
-export default Release;
+export default function Release() {
+  return (
+    <>
+      <div className="gray-bg">
+        <Container>
+          <div className="white-bg">
+            <div className="countdown-text">
+              <h1 className="countdown-headline" style={{ color: "black" }}>
+                We Will Release OSF Academy
+              </h1>       
+              <p>
+                We are working hard and estimated release time products below.
+                Sed ac tristique nunc, ut gravida nunc. Nulla consequat erat non
+                lectus imper
+              </p>{" "}
+            </div>
+            <div>
+              <Countdown
+                timeTillDate="03 03 2021, 9:00 am"
+                timeFormat="MM DD YYYY, h:mm a"
+              />
+              <div className="subscribe-box">
+                <div className="email-submission">
+                  <Form.Control
+                    className="subscribe-form"
+                    size="sm"
+                    type="email"
+                    placeholder="Enter your email to receive latest announcements"
+                  />
+                  <Button className="button-submission">
+                    <FontAwesomeIcon
+                      icon={faAngleRight}
+                      className="submission-sent"
+                      size="lg"
+                    />
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+}
