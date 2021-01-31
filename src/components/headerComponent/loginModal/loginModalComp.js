432af6ba5ff8ca2affee997161e861a2b8b247cf
Deniz <ddogru09@gmail.com>
2021-01-31 18:54:49 +0300

    validation

--- src/components/headerComponent/loginModal/loginModalComp.js
+++ src/components/headerComponent/loginModal/loginModalComp.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
-import LoginFormComp from "./loginFormComp";
 import Form from "react-bootstrap/Form";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { useForm } from "react-hook-form";
 
+const eye = <FontAwesomeIcon icon={faEye} />;
 function MyVerticallyCenteredModal(props) {
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePasswordVisiblity = () => {
+    setPasswordShown(passwordShown ? false : true);
+  };
+
+  const {
+    register,
+    errors,
+    handleSubmit,
+    formState: { isSubmitted },
+  } = useForm();
+  const onSubmit = (values) => console.log(values);
+
   return (
     <Modal
       {...props}
@@ -14,15 +29,68 @@ function MyVerticallyCenteredModal(props) {
     >
       <Modal.Body>
         <h4 className="text-center sign-in-text">Sign in</h4>
-        <div>
-          <LoginFormComp />
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="login-form">
+            <p>Email</p>
+            <div className="pass-wrapper">
+              <input
+                className="email-form"
+                placeholder="E-mail"
+                name="email"
+                ref={register({
+                  required: "Required",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "invalid email address",
+                  },
+                })}
+              />{" "}
+            </div>
+            {errors.email && <p className="error">{errors.email.message}</p>}
+
+            <div className="forgot">
+              <p>Password</p>
+              <p className="green">Forgot Password</p>
+            </div>
+            <div className="pass-wrapper">
+              <input
+                className="email-form"
+                placeholder="Password"
+                name="password"
+                type={passwordShown ? "text" : "password"}
+                ref={register({
+                  required: "Required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must have at least 6 characters",
+                  },
+
+                  pattern: {
+                    pattern: /^0|[1-9]\d*$/,
+                    message:
+                      "Password must contain 1 numeric, 1 uppercase letter, 1 special character",
+                  },
+                })}
+              />
+              <i onClick={togglePasswordVisiblity}>{eye}</i>
+            </div>
+            {errors.password && (
+              <p className="error">{errors.password.message}</p>
+            )}
+
+            <div className="text-center login-button">
+              <Button
+                type="submit"
+                variant="success"
+                onClick={handleSubmit(onSubmit)}
+              >
+                Login
+              </Button>
+            </div>
+          </div>
+        </form>
       </Modal.Body>
-      <div className="text-center login-button">
-        <Button type="submit" variant="success" onClick={props.onHide}>
-          Login
-        </Button>
-      </div>
+
       <Form.Text className="text-muted text-center">
         <p className="green">I don't have an account</p>
       </Form.Text>
